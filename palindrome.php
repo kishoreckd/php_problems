cd82d92e7fca5b87a71721c12923190381a69496
swathiramesh20 <swathisrir@dckap.com>
2023-05-27 12:26:47 +0530

    palindrome bug fixed

--- palindrome.php
+++ palindrome.php
@@ -3,7 +3,7 @@ $GivenInput = readline("Enter Here: ");
 
 $Check_ReveredString = "";
 for($i=strlen($GivenInput)-1;$i>=0;$i--){
-    $Check_ReveredString=$GivenInput[$i];
+    $Check_ReveredString .= $GivenInput[$i];
 }
 if($Check_ReveredString == $GivenInput){
     echo $GivenInput." is a Palindrome .\n";
