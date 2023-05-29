b26ada8be42d447b962aeeeaebe19ae7446e1207
swathiramesh20 <swathisrir@dckap.com>
2023-05-29 11:54:47 +0530

    Triangle problem completed

---
+++ Area_of_Triangle.php
@@ -0,0 +1,15 @@
+<?php
+
+$base =readline("Enter base:");
+$height =readline("Enter Height:");
+
+function Area_Of_Triangle($base,$height)
+{
+    $top =$base * $height;
+    $area = $top/2;
+    echo "Area of Triangle is :$area cm"."\n";
+
+
+}
+
+Area_Of_Triangle($base,$height);
